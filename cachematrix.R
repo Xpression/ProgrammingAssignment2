80a473155695b39243564d057d35a29493b43d7c
Jan-Ove Karlberg <janove.karlberg@gmail.com>
2014-12-06 17:54:49 +0100

    Implemented make cache with unit tests. Added function for running unit tests in cachematrix.R file.

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,49 @@
 
 ## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  if (!class(x) == "matrix") {
+    stop(c("Attempting make from non matrix data:", x))
+  }
+  
+  #Initialization
+  ix <- NULL
+  
+  ## Sets the data of the cached matrix object
+  set <- function(y) {
+    
+    if (!class(y) == "matrix") {
+      stop(c("Attempting to set non matrix data:", y))
+    }
+    
+    x  <<- y
+    ix <<- NULL
+  }
+  
+  ## Gets the data of the cached matrix object
+  get <- function() {
+    return(x)
+  } 
+  
+  ## Sets the inverse of the cahced matrix oject
+  setinverse <- function(inverse) {
+    
+    if (!class(inverse) == "matrix") {
+      stop(c("Attempting to set non matrix inverse data:", inverse))
+    }
+    
+    ix <<- inverse
+  }
+   
+  ## Gets the inverse of the cached matrix object
+  ## NOTE: Returns null when not set
+  getinverse <- function() {
+    return(ix)
+  }
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -14,4 +56,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+
+runTests <- function() {
+  library('RUnit')
+  source('cachematrix.R')
+  
+  test.suite <- defineTestSuite("Cached Matrix",
+                                dirs = file.path("tests"),
+                                testFileRegexp = '*.R')
+  
+  test.result <- runTestSuite(test.suite)
+  printTextProtocol(test.result)
 }
