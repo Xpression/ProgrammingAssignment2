c3d627ff0072040fdaf8a5770b21e37232de8a81
Jan-Ove Karlberg <janove.karlberg@gmail.com>
2014-12-07 14:03:20 +0100

    Reindented with 4 spaces.

--- cachematrix.R
+++ cachematrix.R
@@ -6,49 +6,49 @@
 ## This function creates a special "matrix" object that can cache its inverse.
 ## Not that execution will be stopped if a non matrix object is passed as argument.
 makeCacheMatrix <- function(x = matrix()) {
-  
-  if (!class(x) == "matrix") {
-    stop(c("Attempting make from non matrix data:", x))
-  }
-  
-  #Initialization
-  ix <- NULL
-  
-  ## Sets the data of the cached matrix object and invalidates the cache.
-  set <- function(y) {
     
-    if (!class(y) == "matrix") {
-      stop(c("Attempting to set non matrix data:", y))
+    if (!class(x) == "matrix") {
+        stop(c("Attempting make from non matrix data:", x))
     }
     
-    x  <<- y
-    ix <<- NULL
-  }
-  
-  ## Gets the data of the cached matrix object.
-  get <- function() {
-    return(x)
-  } 
-  
-  ## Sets the inverse of the cached matrix oject.
-  setinverse <- function(inverse) {
+    #Initialization
+    ix <- NULL
     
-    if (!class(inverse) == "matrix") {
-      stop(c("Attempting to set non matrix inverse data:", inverse))
+    ## Sets the data of the cached matrix object and invalidates the cache.
+    set <- function(y) {
+        
+        if (!class(y) == "matrix") {
+            stop(c("Attempting to set non matrix data:", y))
+        }
+        
+        x  <<- y
+        ix <<- NULL
     }
     
-    ix <<- inverse
-  }
-   
-  ## Gets the inverse of the cached matrix object.
-  ## NOTE: Returns null when not set.
-  getinverse <- function() {
-    return(ix)
-  }
-  
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+    ## Gets the data of the cached matrix object.
+    get <- function() {
+        return(x)
+    } 
+    
+    ## Sets the inverse of the cached matrix oject.
+    setinverse <- function(inverse) {
+        
+        if (!class(inverse) == "matrix") {
+            stop(c("Attempting to set non matrix inverse data:", inverse))
+        }
+        
+        ix <<- inverse
+    }
+    
+    ## Gets the inverse of the cached matrix object.
+    ## NOTE: Returns null when not set.
+    getinverse <- function() {
+        return(ix)
+    }
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
@@ -57,31 +57,31 @@ makeCacheMatrix <- function(x = matrix()) {
 ## the matrix has not changed), then the cachesolve should retrieve the 
 ## inverse from the cache. Assume that the matrix supplied is always invertible.
 cacheSolve <- function(x, ...) {
-       
-  currentInverse <- x$getinverse()
-  
-  ## Return cached value if available
-  if(!is.null(currentInverse)) { 
-    return(currentInverse)
-  }
-  
-  ## Calculate inverse and cache it before returning
-  matrix <- x$get()
-  inverse <- solve(matrix)
-  x$setinverse(inverse)
-  
-  return(inverse)
+    
+    currentInverse <- x$getinverse()
+    
+    ## Return cached value if available
+    if(!is.null(currentInverse)) { 
+        return(currentInverse)
+    }
+    
+    ## Calculate inverse and cache it before returning
+    matrix <- x$get()
+    inverse <- solve(matrix)
+    x$setinverse(inverse)
+    
+    return(inverse)
 }
 
 ## Function for running unit tests.
 runTests <- function() {
-  library('RUnit')
-  source('cachematrix.R')
-  
-  test.suite <- defineTestSuite("Cached Matrix",
-                                dirs = file.path("tests"),
-                                testFileRegexp = '*.R')
-  
-  test.result <- runTestSuite(test.suite)
-  printTextProtocol(test.result)
+    library('RUnit')
+    source('cachematrix.R')
+    
+    test.suite <- defineTestSuite("Cached Matrix",
+                                  dirs = file.path("tests"),
+                                  testFileRegexp = '*.R')
+    
+    test.result <- runTestSuite(test.suite)
+    printTextProtocol(test.result)
 }
--- tests/runitCacheMatrix.R
+++ tests/runitCacheMatrix.R
@@ -2,80 +2,84 @@
 
 ## Test initialization 
 test.makeCacheMatrixInitialization <- function() {
-  
-  ## makeCacheMatrix should throw exception if non matrix argument is given.
-  checkException(makeCacheMatrix(NULL))
-  
-  ## makeCacheMatrix should be initalized to contain empty matrix if no argument given.
-  checkEquals(matrix(), makeCacheMatrix()$get())
-  
-  ## makeCacheMatrix should be correctly initialized when argument is specified.
-  a <- matrix(1:20, 4, 5)
-  checkEquals(a, makeCacheMatrix(a)$get())
-  
-  ## makeCacheMatrix should initialize the inverse to NULL.
-  checkEquals(NULL, makeCacheMatrix()$getinverse())
+    
+    ## makeCacheMatrix should throw exception if non matrix argument is given.
+    checkException(makeCacheMatrix(NULL))
+    
+    ## makeCacheMatrix should be initalized to contain empty matrix if no 
+    ## argument given.
+    checkEquals(matrix(), makeCacheMatrix()$get())
+    
+    ## makeCacheMatrix should be correctly initialized when argument is specified.
+    a <- matrix(1:20, 4, 5)
+    checkEquals(a, makeCacheMatrix(a)$get())
+    
+    ## makeCacheMatrix should initialize the inverse to NULL.
+    checkEquals(NULL, makeCacheMatrix()$getinverse())
 }
 
 ## Test behavior of set function
 test.makeCacheMatrix_set <- function() {
-  
-  ## set function should throw exception if non matrix argument is given.
-  a <- makeCacheMatrix()
-  checkException(a$set(NULL))
-  
-  ## set function should correctly set data if argument is valid.
-  b <- matrix(1:20, 4, 5)
-  a$set(b)
-  checkEquals(b, a$get())
-  
-  ## set function should set inverse value to NULL.
-  c <- matrix(20:40, 4, 5)
-  a$setinverse(c)
-  a$set(b)
-  checkEquals(NULL, a$getinverse())
+    
+    ## set function should throw exception if non matrix argument is given.
+    a <- makeCacheMatrix()
+    checkException(a$set(NULL))
+    
+    ## set function should correctly set data if argument is valid.
+    b <- matrix(1:20, 4, 5)
+    a$set(b)
+    checkEquals(b, a$get())
+    
+    ## set function should set inverse value to NULL.
+    c <- matrix(20:40, 4, 5)
+    a$setinverse(c)
+    a$set(b)
+    checkEquals(NULL, a$getinverse())
 }
 
 ## Test behavior of setinverse function
 test.makeCacheMatrix_setinverse <- function() {
-  
-  ## setinverse function should throw exception if non matrix argument is given.
-  a <- makeCacheMatrix()
-  checkException(a$setinverse(NULL))
-  
-  ## setinverse function should correctly set inverse matrix value if argument is valid.
-  b <- matrix(1:20, 4, 5)
-  a$setinverse(b)
-  checkEquals(b, a$getinverse())
+    
+    ## setinverse function should throw exception if non matrix argument is given.
+    a <- makeCacheMatrix()
+    checkException(a$setinverse(NULL))
+    
+    ## setinverse function should correctly set inverse matrix value if argument 
+    ## is valid.
+    b <- matrix(1:20, 4, 5)
+    a$setinverse(b)
+    checkEquals(b, a$getinverse())
 }
 
 ## Test behavior of getinverse function
 test.makeCacheMatrix_getinverse <- function() {
-  
-  ## getinverse function should return NULL in no inverse matrix set.
-  a <- makeCacheMatrix()
-  checkEquals(NULL, a$getinverse())
-  
-  ## getinverse function should return inverse matrix if set.
-  b <- matrix(1:20, 4, 5)
-  a$setinverse(b)
-  checkEquals(b, a$getinverse())
+    
+    ## getinverse function should return NULL in no inverse matrix set.
+    a <- makeCacheMatrix()
+    checkEquals(NULL, a$getinverse())
+    
+    ## getinverse function should return inverse matrix if set.
+    b <- matrix(1:20, 4, 5)
+    a$setinverse(b)
+    checkEquals(b, a$getinverse())
 }
 
 ## Test behavior of cacheSolve function
 test.cacheSolve <- function() {
     
-  ## cacheSolve should return the inverse matrix, and cache it if not allready calculated.
-  a <- matrix(c(1, -1, 1, 2), 2, 2)
-  ca = makeCacheMatrix(a)
-  checkEquals(solve(a), cacheSolve(ca))
-  
-  ## cacheSolve should return the inverse matrix from the cache if allready calculated.
-  checkEquals(solve(a), cacheSolve(ca))
-  
-  ## if the matrix has changed, i.e. the cached copy is stale, it should be updated before
-  ## the inverse matrix is returned.
-  b <- matrix(c(1, 0, 5, 2, 1, 6, 3, 4, 0), 3, 3)
-  ca$set(b)
-  checkEquals(solve(b), cacheSolve(ca))
+    ## cacheSolve should return the inverse matrix, and cache it if not allready 
+    ## calculated.
+    a <- matrix(c(1, -1, 1, 2), 2, 2)
+    ca = makeCacheMatrix(a)
+    checkEquals(solve(a), cacheSolve(ca))
+    
+    ## cacheSolve should return the inverse matrix from the cache if allready 
+    ## calculated.
+    checkEquals(solve(a), cacheSolve(ca))
+    
+    ## if the matrix has changed, i.e. the cached copy is stale, it should be 
+    ## updated before the inverse matrix is returned.
+    b <- matrix(c(1, 0, 5, 2, 1, 6, 3, 4, 0), 3, 3)
+    ca$set(b)
+    checkEquals(solve(b), cacheSolve(ca))
 }
